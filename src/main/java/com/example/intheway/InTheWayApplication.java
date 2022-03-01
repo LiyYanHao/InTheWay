39ecf0090870ff33dbe00f69ea5da0362aff0ae5
liyanhao <1779077919@qq.com>
2022-03-02 02:11:28 +0800

    新建vue项目

---
+++ src/main/java/com/example/intheway/InTheWayApplication.java
@@ -0,0 +1,13 @@
+package com.example.intheway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InTheWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InTheWayApplication.class, args);
+    }
+
+}
